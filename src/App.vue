8781b41c6b9b13c5062772cc5451fc55fa93fc3a
NewGuitarWhoDis <conorwestley@gmail.com>
2023-04-08 23:21:52 +0100

    cors

--- src/App.vue
+++ src/App.vue
@@ -99,10 +99,30 @@ function saveByteArrayToFile(byteArray, fileName, fileType) {
     // URL.revokeObjectURL(url);
 }
 
-async function getVersion() {
-  const URL = await fetch('https://clickit.conorwestley.co.uk/');
-  URL.json()
-  console.log(URL.headers.get('version'));
+function getVersion() {
+  var txt = "";
+  // sample url used here, you can make it more dynamic as per your need.
+  // used AJAX here to just hit the url & get the page source from those website. It's used here like the way CURL or file_get_contents (https://www.php.net/manual/en/function.file-get-contents.php) from PHP used to get the page source.
+  $.ajax({
+      url: "https://jsfiddle.net/",
+			error: function() {
+				txt = "Unable to retrieve webpage source HTML";
+			}, 
+      success: function(response){
+          // will get the output here in string format
+          // used $.parseHTML to get DOM elements from the retrieved HTML string. Reference: https://api.jquery.com/jquery.parsehtml/
+          response = $.parseHTML(response);
+          $.each(response, function(i, el){
+              if(el.nodeName.toString().toLowerCase() == 'meta' && $(el).attr("name") != null && typeof $(el).attr("name") != "undefined"){
+                  txt += $(el).attr("name") +"="+ ($(el).attr("content")?$(el).attr("content"):($(el).attr("value")?$(el).attr("value"):"")) +"<br>";
+                  console.log($(el).attr("name") ,"=", ($(el).attr("content")?$(el).attr("content"):($(el).attr("value")?$(el).attr("value"):"")), el);
+              }
+          });
+      },
+      complete: function(){
+          console.log(txt);
+      }
+  });
 }
 
 </script>
